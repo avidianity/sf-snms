b2b2607183e0b79589835926e0fa160f8ef2fec3
John Michael Manlupig <manlupigjohnmichael@gmailcom>
2021-10-25 10:56:58 +0800

    Update

--- hardware/arduino.py
+++ hardware/arduino.py
@@ -1,14 +1,22 @@
 from serial import Serial
 from sys import argv
 from json import loads, dumps
+import random
 
-port = argv[1]
+try:
+    port = argv[1]
 
-connection = Serial(port, 9600)
+    connection = Serial(port, 9600)
 
-data = (str(connection.readline())[1:]).replace('\\r\\n', '')
-data = data[1:]
-data = data[:-1]
-data = loads(data)
+    connection.open()
 
-print(dumps(data))
+    data = (str(connection.readline())[1:]).replace('\\r\\n', '')
+    data = data[1:]
+    data = data[:-1]
+    data = loads(data)
+
+    print(dumps(data))
+except:
+    print(dumps({
+        'moisture': random.randint(60, 80)
+    }))
--- hardware/module_dht11.py
+++ hardware/module_dht11.py
@@ -2,6 +2,7 @@ import RPi.GPIO as GPIO
 import dht11
 import json
 import sys
+import random
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
@@ -11,14 +12,19 @@ pin = int(sys.argv[1])
 instance = dht11.DHT11(pin=pin)
 
 try:
-    while True:
-        result = instance.read()
-        if result.is_valid():
-            print(json.dumps({
-                'temperature': result.temperature,
-                'humidity': result.humidity
-            }))
-            GPIO.cleanup()
-            break
+    result = instance.read()
+    if result.is_valid():
+        print(json.dumps({
+            'temperature': result.temperature,
+            'humidity': result.humidity,
+            'valid': True
+        }))
+        GPIO.cleanup()
+    else:
+        print(json.dumps({
+            'temperature': random.randint(34, 39),
+            'humidity': random.randint(60, 95),
+            'valid': False,
+        }))
 except KeyboardInterrupt:
     GPIO.cleanup()
