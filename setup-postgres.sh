619c3fc62258f26a79b27b42cea6e853998ca5f7
John Michael Manlupig <manlupigjohnmichael@gmail.com>
2021-11-17 14:17:39 +0800

    Update

--- setup-postgres.sh
+++ setup-postgres.sh
@@ -10,6 +10,6 @@ sudo -u postgres createuser "$DATABASE_USERNAME"
 
 sudo -u postgres createdb "$DATABASE_NAME"
 
-sudo su -c "psql -c \"ALTER USER $DATABASE_USERNAME WITH ENCRYPTED PASSWORD $DATABASE_PASSWORD;\"" postgres
+sudo su -c "psql -c \"ALTER USER $DATABASE_USERNAME WITH ENCRYPTED PASSWORD '$DATABASE_PASSWORD';\"" postgres
 
 sudo su -c "psql -c \"GRANT ALL PRIVILEGES ON DATABASE $DATABASE_NAME TO $DATABASE_USERNAME;\"" postgres
